7afdba68f3d6dead98a4a14d10f00c426e464e78
Unknown <wagner_gcastro@hotmail.com>
2020-12-04 06:28:17 -0300

    #08 React Route e React Route Dom
    - Organizando as rotas para novas paginas e components

---
+++ src/views/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Home extends React.Component {
+    render() {
+        return (
+            <section className="contato">
+                <h3>Home</h3>
+                <p>Esta é a página de Home</p>
+            </section>  
+        )
+    }
+}
+
+export default Home;
---
+++ src/views/Sobre.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Home extends React.Component {
+    render() {
+        return (
+            <section className="contato">
+                <h3>Sobre</h3>
+                <p>Esta é a página de Sobre</p>
+            </section>  
+        )
+    }
+}
+
+export default Home;
